376eadaa8768e312b0e734316a482a16840b7ed5
Dinh <dinhhuynh21@gmail.com>
2019-12-24 11:46:35 +0700

    Edit procfile

--- PythonWeb1/urls.py
+++ PythonWeb1/urls.py
@@ -21,26 +21,6 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'admin/password_reset/',
-        auth_views.PasswordResetView.as_view(),
-        name='admin_password_reset',
-        ),
-    path(
-        'admin/password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done',
-        ),
-    path(
-        'reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm',
-        ),
-    path(
-        'reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete',
-        ),
     path('',include('home.urls')),
     path('blog/', include('blog.urls')),
     path('student/',include('student.urls'))
