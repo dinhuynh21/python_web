2465a5166fbb9b38ef082c229181ab63af940b59
Dinh <dinhhuynh21@gmail.com>
2019-12-24 15:27:57 +0700

    edit html view

--- home/views.py
+++ home/views.py
@@ -8,19 +8,19 @@ from blog.models import Post
 # Create your views here.
 def index(request):
     form = CreationForm()
-    post= Post.objects.all().order_by("-date")
-    paginator = Paginator(post,4)
-    page = request.GET.get('page')
-    contacts = paginator.get_page(page)
+    #post= Post.objects.all().order_by("-date")
+    #paginator = Paginator(post,4)
+    #page = request.GET.get('page')
+    #contacts = paginator.get_page(page)
     if request.method == 'POST':
         form = CreationForm(request.POST)
         alert=False
         if form.is_valid():
             form.save()
             alert=True
-            return render(request, 'pages/home.html',{'alert':alert,'post':contacts})
+            return render(request, 'pages/home.html',{'alert':alert})#,'post':contacts})
         return HttpResponse("Dữ liệu không hợp lệ")
-    return render(request, 'pages/home.html', {'form':form, 'post':contacts})
+    return render(request, 'pages/home.html', {'form':form})#, 'post':contacts})
 def contact(request):
     return render(request, 'pages/contact.html')
 def error(request):
