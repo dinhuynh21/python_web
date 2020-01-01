2e8e59ee9eaeef1037f0f4239768aee5febb9ceb
Dinh <dinhhuynh21@gmail.com>
2020-01-01 20:31:28 +0700

    rechange DEBUG

--- PythonWeb1/settings.py
+++ PythonWeb1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1^c@&#^&-bte$z^c%46ws!-g8yeu$8e^k$09qvkhvdgvdzp8ct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
